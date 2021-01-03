9c333c49297b2c7607f050825b06bb839163da7f
Aditya Bodhankar <adityaabodhankar@gmail.com>
2021-01-03 17:53:24 +0530

    profile image

--- app/src/main/java/com/aditya/quizapplication/EditProfileActivity.java
+++ app/src/main/java/com/aditya/quizapplication/EditProfileActivity.java
@@ -3,16 +3,20 @@ package com.aditya.quizapplication;
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.aditya.quizapplication.Models.ModelUser;
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,9 +24,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.HashMap;
 import java.util.Objects;
+import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -35,7 +42,9 @@ public class EditProfileActivity extends AppCompatActivity {
     FirebaseAuth auth;
     DatabaseReference reference;
     FirebaseUser user;
+    StorageReference mStorageRef;
     ProgressDialog progressDialog;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +59,7 @@ public class EditProfileActivity extends AppCompatActivity {
         }
         user = auth.getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
         nameInput = findViewById(R.id.editProfileName);
         phoneInput = findViewById(R.id.editProfilePhone);
@@ -93,6 +103,13 @@ public class EditProfileActivity extends AppCompatActivity {
                 enableInputs();
             }
         });
+
+        iconInput.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(intent,1);
+        });
     }
 
     private void saveData() {
@@ -113,13 +130,6 @@ public class EditProfileActivity extends AppCompatActivity {
         reference.child("Users").child(user.getUid()).updateChildren(data).addOnCompleteListener(task -> Toast.makeText(EditProfileActivity.this, "Profile Updated SuccessFully", Toast.LENGTH_SHORT).show()).addOnFailureListener(e -> Toast.makeText(EditProfileActivity.this, "Error => "+e.getMessage(), Toast.LENGTH_SHORT).show());
     }
 
-    @SuppressLint("SetTextI18n")
-    private void enableInputs() {
-        nameInput.setEnabled(true);
-        phoneInput.setEnabled(true);
-        saveButton.setText("SAVE");
-    }
-
     private void setData(ModelUser value) {
         nameInput.setText(value.getName());
         phoneInput.setText(value.getPhone());
@@ -130,9 +140,52 @@ public class EditProfileActivity extends AppCompatActivity {
     }
 
     @SuppressLint("SetTextI18n")
+    private void enableInputs() {
+        nameInput.setEnabled(true);
+        phoneInput.setEnabled(true);
+        saveButton.setText("SAVE");
+    }
+
+    @SuppressLint("SetTextI18n")
     private void disableInputs() {
         nameInput.setEnabled(false);
         phoneInput.setEnabled(false);
         saveButton.setText("EDIT");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && resultCode==RESULT_OK && data!=null && data.getData()!=null){
+            imageUri = data.getData();
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Image Uploading");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+            final String randomkey = UUID.randomUUID().toString();
+            StorageReference uploadImage = mStorageRef.child("images/"+randomkey);
+
+            uploadImage.putFile(imageUri)
+                    .addOnSuccessListener(taskSnapshot -> {
+                        progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(), "Image Uploaded", Toast.LENGTH_SHORT).show();
+                        Task<Uri> task = Objects.requireNonNull(Objects.requireNonNull(taskSnapshot.getMetadata()).getReference()).getDownloadUrl();
+                        task.addOnSuccessListener(uri -> {
+                            String imageUrl=uri.toString();
+                            reference = FirebaseDatabase.getInstance().getReference("Users").child(user.getUid());
+                            reference.child("profileImage").setValue(imageUrl);
+                        });
+
+                    })
+                    .addOnFailureListener(exception -> {
+                        progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(), "Failed to upload"+exception, Toast.LENGTH_SHORT).show();
+                    }).addOnProgressListener(snapshot -> {
+                double progresspercent = (100.00 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                progressDialog.setMessage("Uploading :- "+(int)progresspercent+" %");
+            });
+        }else{
+            Toast.makeText(this, "Operation Cancelled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
