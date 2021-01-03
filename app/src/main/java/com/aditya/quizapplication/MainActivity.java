ff9d4e476aa4a9145edfaf122ebb069967d0adab
Aditya Bodhankar <adityaabodhankar@gmail.com>
2021-01-03 10:02:18 +0530

    Login Layout Created

---
+++ app/src/main/java/com/aditya/quizapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.aditya.quizapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    FirebaseUser user;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser()==null){
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+            return;
+        }
+        //do other code
+        user = auth.getCurrentUser();
+
+    }
+}
