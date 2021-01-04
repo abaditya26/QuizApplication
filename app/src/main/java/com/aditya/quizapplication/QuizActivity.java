0f638e62f29b706ea4ca7b9f26d9774f00d8fb2f
Aditya Bodhankar <adityaabodhankar@gmail.com>
2021-01-04 19:46:47 +0530

    quiz activity

--- app/src/main/java/com/aditya/quizapplication/QuizActivity.java
+++ app/src/main/java/com/aditya/quizapplication/QuizActivity.java
@@ -4,9 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aditya.quizapplication.Models.ModelQuestions;
 import com.google.firebase.auth.FirebaseAuth;
@@ -32,6 +35,8 @@ public class QuizActivity extends AppCompatActivity {
     DatabaseReference reference;
     int position = 0;
 
+    String selectedText = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +58,7 @@ public class QuizActivity extends AppCompatActivity {
         }
         reference = FirebaseDatabase.getInstance().getReference();
         questionsList = new ArrayList<>();
+        selected = new ArrayList<>();
 
         reference.child("Quiz").child(quizId).child("Questions").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -79,6 +85,59 @@ public class QuizActivity extends AppCompatActivity {
         option2Button.setText(questionsList.get(position).getOption_2());
         option3Button.setText(questionsList.get(position).getOption_3());
         option4Button.setText(questionsList.get(position).getOption_4());
+        index.setText((position+1)+"/"+questionsList.size());
+        selectedText = "";
     }
 
+    private void setColorDefault() {
+        option1Button.setTextColor(Color.WHITE);
+        option2Button.setTextColor(Color.WHITE);
+        option3Button.setTextColor(Color.WHITE);
+        option4Button.setTextColor(Color.WHITE);
+    }
+
+    public void selectOption1(View view) {
+        selectedText = option1Button.getText().toString();
+        setColorDefault();
+        option1Button.setTextColor(Color.GREEN);
+    }
+
+    public void selectOption2(View view) {
+        selectedText = option2Button.getText().toString();
+        setColorDefault();
+        option2Button.setTextColor(Color.GREEN);
+    }
+
+    public void selectOption3(View view) {
+        selectedText = option3Button.getText().toString();
+        setColorDefault();
+        option3Button.setTextColor(Color.GREEN);
+    }
+
+    public void selectOption4(View view) {
+        selectedText = option4Button.getText().toString();
+        setColorDefault();
+        option4Button.setTextColor(Color.GREEN);
+    }
+
+    public void nextClicked(View view) {
+        if (selectedText.equals("")){
+            Toast.makeText(this, "Select a option first", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        selected.add(selectedText);
+        if (position==questionsList.size()-1){
+            for (String sample : selected){
+                System.out.println(sample);
+            }
+            Toast.makeText(this, "All Questions Attempted", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        setColorDefault();
+        position++;
+        setData(position);
+        if (position==questionsList.size()-1){
+            ((Button)findViewById(R.id.buttonNext)).setText("SUBMIT");
+        }
+    }
 }
